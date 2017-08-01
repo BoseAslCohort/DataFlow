476ca4cf1983fc9e3f1ec22a5fd67ded4c46ca09
Isaac Julien <isaacjulien@Isaacs-MacBook-Pro.local>
2017-08-01 11:39:33 -0700

    first

--- Scratch.py
+++ Scratch.py
@@ -6,8 +6,7 @@ Isaac Julien
 
 import apache_beam as beam
 
-#import apache_beam.io.filesystems as abfs
-import apache_beam.io.gcp.gcsfilesystem.GCSFileSystem as gcsio
+import apache_beam.io.tfrecordio as tfrecordio
 
 import argparse
 
@@ -15,7 +14,9 @@ if __name__ == '__main__':
 
    parser = argparse.ArgumentParser(description='DataFlow for Video-Level data augmentation')
 
-   parser.add_argument('--inputdir', default='gs://youtube8m-ml-us-east1/1/frame_level/train/', help='Input directory')
+   #parser.add_argument('--inputdir', default='gs://youtube8m-ml-us-east1/1/frame_level/train/', help='Input directory')
+   parser.add_argument('--inputdir', default='gs://testinput/input/', help='Input directory')
+
    parser.add_argument('--outputdir', default='gs://processedframeleveloutput/outputs/', help='Output directory')
 
    options, pipeline_args = parser.parse_known_args()
@@ -26,7 +27,7 @@ if __name__ == '__main__':
    output_prefix = options.outputdir + "test"
 
    (p
-      | 'GetFilenames' >> gcsio.match(input)
+      | 'GetFilenames' >> tfrecordio.Read(input)
       | 'write' >> beam.io.WriteToText(output_prefix)
    )
 
